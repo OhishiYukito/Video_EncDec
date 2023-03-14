e57e45761e93892b94bb2fa1713b34f8799905dd
EmacsPython <ohishi.yukito.497@s.kyushu-u.ac.jp>
2023-03-14 13:57:55 +0900

    update demo.py for Conv2+1D

--- demo.py
+++ demo.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 import tools.initial_process as init
 import tools.show_functions as show
 ### Parameters ###############################################
-subject_id = 0
+subject_id = 4
 subjects = {
     0 : "reconstruction",
     1 : "classification",
@@ -20,6 +20,13 @@ input_H = 192
 input_W = 256
 batch_size = 8
 lab_server_pc = True
+
+
+base_model_id = 1
+folder_name_list = {
+    0 : "Conv3d",       # if base_model is Conv3d
+    1 : "(2+1)D",       # if base_model is Conv2Plus1D
+}
 ##########################################################
 
 
@@ -31,12 +38,13 @@ class_indxs, device, dataloader = init.initial_process(lab_server_pc,
                                                        train=False)
 
 # load models
-folder_name = str(input_H)+'*'+str(input_W)
+folder_name = folder_name_list[base_model_id]
+input_shape = str(input_H)+'*'+str(input_W)
 folder_path = 'result/' + folder_name
-encoder = torch.load(folder_path +'/'+ subjects[subject_id]+'_encoder_'+folder_name+'.pth')
-decoder = torch.load(folder_path +'/'+ subjects[subject_id]+'_decoder_'+folder_name+'.pth')
-print("encoder : ", subjects[subject_id]+'_encoder_'+folder_name+'.pth')
-print("decoder : ", subjects[subject_id]+'_decoder_'+folder_name+'.pth')
+encoder = torch.load(folder_path +'/'+ subjects[subject_id]+'_encoder_'+input_shape+'.pth')
+decoder = torch.load(folder_path +'/'+ subjects[subject_id]+'_decoder_'+input_shape+'.pth')
+print("encoder : ", folder_path +'/'+ subjects[subject_id]+'_encoder_'+input_shape+'.pth')
+print("decoder : ", folder_path +'/'+ subjects[subject_id]+'_decoder_'+input_shape+'.pth')
 
 if getattr(encoder, 'device_ids', False):
     encoder = encoder.module
